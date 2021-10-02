870214db0c1a9d00a6bcc84f514b66d013ed943d
kanavsingla28 <kanavsingla28@gmail.com>
2021-10-02 12:04:18 +0530

    Trip final & documentation

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 
 const connect = ()=>{
-    return mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.10pqn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+    return mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.10pqn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
 }
 app.use(cors());
 app.use(express.json());
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
 },{
     versionKey:false,
     timestamps:true
-})
+});
 const User = new mongoose.model("user",userSchema);
 //------------- Users Schema End -------------//
 
@@ -31,7 +31,7 @@ const citySchema = new mongoose.Schema({
 },{
     versionKey:false,
     timestamps:true
-})
+});
 const City = mongoose.model("city",citySchema)
 //------------- City Schema End -------------//
 //------------- Cars Schema Start -------------//
@@ -50,7 +50,7 @@ const carsSchema = new mongoose.Schema({
 },{
     versionKey:false,
     timestamps:true
-})
+});
 const Cars = mongoose.model("car",carsSchema)
 //------------- Cars Schema End -------------//
 
@@ -61,17 +61,74 @@ const tripSchema = mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref:"user", required:true},
     pick_up_date:{type: Date, required:true},
     drop_off_date:{type:Date, required:true}
-})
+},{
+    versionKey:false,
+    timestamps:true
+});
+const Trip = mongoose.model("trip",tripSchema);
 //------------- Trips Schema End -------------//
 
 app.get("/",(req,res)=>{
     res.send({"All Endpoints":"/",
-    "GET Cities":"/cities",
-    "GET Search Cities":"/cities?name=<city name>",
-    "GET Cars":"/cars",
-    "GET Cars by City":"/cars/city/<city name>"
-    })
+    "Users":{
+        "POST: User Sign Up":{
+            "Endpoint":"/user/signup",
+            "Demo Body":{
+                "first_name":"Kanav",
+                "last_name":"Singla",
+                "email":"name@gmail.com",
+                "password":"123",
+                "phone_num":"9987654321"
+            }
+        },
+        "POST: User Login" : {
+            "Endpoint":"/user/login",
+            "Demo Body":{
+                "email":"name@gmail.com",
+                "password":"123"
+            }
+        }
+    },
+    "Cities":{
+        "GET: All Cities":"/cities",
+        "GET: Search City name":"/cities?name=<city name>"
+    },
+    "Cars":{
+        "GET: All cars":"/cars",
+        "GET: Cars by City":"/cars/city/<city id>"
+    },
+    "Trips":{
+        "POST: New Trip":{
+            "Endpoint": "/trips",
+            "Demo Body":{
+                "car":"615584d6c1a3375b584fe0f2",
+                "user":"6157380d303421fdb80a1ad8",
+                "pick_up_date":"2021-10-02",
+                "drop_off_date":"2021-10-07"
+            }
+        },
+        "GET: Trips by user id": "/trips/<User Id>"
+    }
+    
+    });
 })
+//------------- Trip CRUD OPERATIONS Start -------------//
+// Get Trip by user id
+app.get("/trips/:userid", async (req,res)=>{
+    const trip = await Trip.find({user:{$eq:req.params.userid}}).populate("car").populate({
+        path:"user",
+        select:["first_name","last_name","phone_num"]
+    }).lean().exec();
+    return res.status(200).send(trip);
+})
+// Create new Trip
+app.post("/trips", async (req,res)=>{
+    const trip = await Trip.create(req.body);
+    return res.status(201).send(trip);
+});
+//------------- Trip CRUD OPERATIONS End -------------//
+
+
 //------------- User CRUD OPERATIONS Start -------------//
 // Create new User
 app.post("/user/signup", async (req,res)=>{
@@ -89,7 +146,7 @@ app.post("/user/signup", async (req,res)=>{
     }else{
         return res.status(201).send({"status":"error","details":"User Already Exists"});
     }
-})
+});
 app.post("/user/login", async(req,res)=>{
     const existingUser = await User.find({$and:[{email:{$eq:req.body.email}} ,{password:{$eq:req.body.password}}]})
     if(existingUser.length!=0){
@@ -97,10 +154,8 @@ app.post("/user/login", async(req,res)=>{
     }else{
         return res.status(201).send({"status":"error","details":"Invalid Credentials"});
     }
-})
+});
 //------------- User CRUD OPERATIONS End -------------//
-
-
 
 //------------- City CRUD OPERATIONS Start -------------//
 // Get All cities
@@ -115,27 +170,27 @@ app.get("/cities", async (req,res)=>{
         cities = await City.find().lean().exec();
     }
     return res.status(200).send(cities);
-})
+});
 // Create new city
 app.post("/cities", async (req,res)=>{
     const city = await City.create(req.body);
     return res.status(201).send(city);
-})
+});
 // Get City by Id
 app.get("/cities/:id", async(req,res)=>{
     const city = await City.findById(req.params.id).lean().exec();
     return res.status(200).send(city);
-})
+});
 // Update City by Id
 app.patch("/cities/:id", async(req,res)=>{
     const city = await City.findByIdAndUpdate(req.params.id,req.body,{new:true});
     return res.status(200).send(city);
-})
+});
 // Delete City by Id
 app.delete("/cities/:id", async(req,res)=>{
     const city = await City.findByIdAndDelete(req.params.id);
     return res.status(200).send(`City with id ${req.params.id} deleted`)
-})
+});
 //------------- City CRUD OPERATIONS End -------------//
 
 
@@ -145,17 +200,17 @@ app.delete("/cities/:id", async(req,res)=>{
 app.get("/cars", async (req,res)=>{
     const cars = await Cars.find().lean().exec();
     return res.status(200).send(cars);
-})
+});
 // Create new Car
 app.post("/cars", async (req,res)=>{
     const cardata = await Cars.create(req.body);
     return res.status(201).send(cardata);
-})
+});
 // Get Car by Id
 app.get("/cars/:id", async(req,res)=>{
     const cardata = await Cars.findById(req.params.id).lean().exec();
     return res.status(200).send(cardata);
-})
+});
 // Get Car by City
 app.get("/cars/city/:city", async(req,res)=>{
     const cardata = await Cars.find({city:{$eq:req.params.city}}).populate({
@@ -163,17 +218,17 @@ app.get("/cars/city/:city", async(req,res)=>{
         select:["city","state","pickup_address"]
     }).lean().exec();
     return res.status(200).send(cardata);
-})
+});
 // Update Car by Id
 app.patch("/cars/:id", async(req,res)=>{
     const cardata = await Cars.findByIdAndUpdate(req.params.id,req.body,{new:true});
     return res.status(200).send(cardata);
-})
+});
 // Delete Car by Id
 app.delete("/cars/:id", async(req,res)=>{
     const cardata = await Cars.findByIdAndDelete(req.params.id);
     return res.status(200).send(`Car with id ${req.params.id} deleted`)
-})
+});
 //------------- Car CRUD OPERATIONS End -------------//
 
 
@@ -185,4 +240,4 @@ app.delete("/cars/:id", async(req,res)=>{
 app.listen(process.env.PORT , async ()=>{
     await connect();
     console.log(`App working on port ${process.env.PORT}`);
-})
+});
